ba7687d8a7fc3a0e99c4131409664e928a5f90ec
carlosmyr23 <carlosmyr23@gmail.com>
2020-01-29 13:21:20 -0600

    Update fedora_tools_bash.sh

--- fedora_tools_bash.sh
+++ fedora_tools_bash.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+# ref: https://github.com/sgan81/apfs-fuse
+sudo dnf upgrade 
 sudo dnf install zlib bzip2 libattr cmake gcc-c++ git fuse3-devel
+sudo dnf upgrade
+git clone https://github.com/sgan81/apfs-fuse.git
+cd apfs-fuse
+git submodule init
+git submodule update
+mkdir build
+cd build
+cmake ..
+make
+# Then follow the instructions in https://github.com/sgan81/apfs-fuse
